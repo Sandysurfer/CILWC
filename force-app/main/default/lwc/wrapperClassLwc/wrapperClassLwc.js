9dbf5dd49f927cfb1e3b8ca3788f99b6ee70096d
Sandysurfer <115895789+Sandysurfer@users.noreply.github.com>
2023-09-28 22:48:31 +0530

    wra

--- force-app/main/default/lwc/wrapperClassLwc/wrapperClassLwc.js
+++ force-app/main/default/lwc/wrapperClassLwc/wrapperClassLwc.js
@@ -1,10 +1,10 @@
 import { LightningElement, wire, track } from 'lwc';
 import getAllAccountWithContacts from '@salesforce/apex/AccountContactController.getAllAccountWithContacts'
-import createAccountContact from '@salesforce/apex/AccountLightningService.createAccountContact';
+import createAccountContact from '@salesforce/apex/AccountWrapperService.createAccountContact';
 
 export default class WrapperClassDemo extends LightningElement {
 
-    //Sending Wrapper From Apex to Lwc Object....
+    //Sending Wrapper Class From Apex to Lwc Object....
 
     @track accountsWithContacts;
     @track error;
@@ -17,28 +17,27 @@ export default class WrapperClassDemo extends LightningElement {
             console.error('Error--->', error);
         }
     }
-
-    //Sending Object from Lwc to Apex Wrapper (Using Imperative method)......
+    //Creating Account With related Contacts by,
+    //Sending Object data from Lwc to Apex Wrapper Class,(Using Imperative method)......
     contacts = [];
+    createdAccount;
     errorRecord;
 
     handleClick() {
-        var conData =
-        {
+        var conData = {
             LastName: 'haldiram',
             Email: 'haldiram@gmail.com',
             Phone: '546854685151'
         };
         this.contacts.push(conData);
 
-        let accData =
-        {
+        let accData = {
             Name: 'NagpurFoods',
             NoOfContacts: 2,
             Contacts: this.conData
         };
 
-        createAccountContact({ accWrapper : accData })
+        createAccountContact({ accWrapper: accData })
             .then(result => {
                 this.createdAccount = result;
                 console.log('Data:' + JSON.stringify(this.createdAccount));
